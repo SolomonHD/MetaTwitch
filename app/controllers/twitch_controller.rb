f813aea2d5d6b5b600b28a0538f72bf8247424ec
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-03-07 12:53:44 -0500

    Ported twitch dcode over some tests added

---
+++ app/controllers/twitch_controller.rb
@@ -0,0 +1,22 @@
+class TwitchController < ApplicationController
+  def index
+    @response = HTTParty.get("https://api.twitch.tv/kraken/search/channels?q=dota2&limit=25",
+    :headers => {
+      "Client Secret" => "#{ENV['TWITCH_TOKEN']}",
+      "Client-ID" => "ec5agpzfzyczq1wskui7hv9zzrhageg"})
+  end
+
+  def show
+    @response = HTTParty.get("https://api.twitch.tv/kraken/search/channels?q=#{params[:id]}&limit=25")
+    #  :headers => {
+    #    "Client Secret" => "#{ENV['TWITCH_TOKEN']}",
+    #    "Client-ID" => "ec5agpzfzyczq1wskui7hv9zzrhageg"})
+    if @response["_total"].blank?
+      redirect_to root_path
+    end
+    @ign_response = HTTParty.get("https://videogamesrating.p.mashape.com/get.php?game=#{params[:id]}",
+    :headers => {
+      "X-Mashape-Key" => "8jxjMfitOnmshjPtuOrBNALxx19mp1RoJOojsnkNMwm5lykf2f"
+      })
+    end
+end
---
+++ app/helpers/ign_helper.rb
@@ -0,0 +1,2 @@
+module IgnHelper
+end
